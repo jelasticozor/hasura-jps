ab535b2c4e0e95cef2fb8faa51f92ed73be5a405
Laurent Michel <softozor@gmail.com>
2021-09-05 15:58:47 +0200

    return empty dict when success_text is none

--- features/utils/manifest_data.py
+++ features/utils/manifest_data.py
@@ -1,9 +1,8 @@
 def get_manifest_data(success_text):
+    if not success_text:
+        return {}
+
     manifest_data = {}
-
-    if success_text is None:
-        return manifest_data
-
     for key_value in success_text.split('\n'):
         key, value = key_value.split(':')
         key = key.replace('<strong>', '')
