20bfc411d5c2c5d5b1e7b99802e5be5113d0961f
Laurent Michel <softozor@gmail.com>
2021-09-19 13:27:16 +0200

    fix todos table creation

--- features/data/database/todo_project/migrations/default/1631798843392_create_table_public_todos/up.sql
+++ features/data/database/todo_project/migrations/default/1631798843392_create_table_public_todos/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "public"."todos" (
-    "id" uuid NOT NULL SET DEFAULT uuid_generate_v4(),
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "title" varchar NOT NULL,
     "description" text NOT NULL,
     "state" text NOT NULL DEFAULT 'NEW',
-    ADD CONSTRAINT "todos_state_fkey" FOREIGN KEY ("state") REFERENCES "public"."states" ("state") ON UPDATE RESTRICT ON DELETE RESTRICT,
+    FOREIGN KEY ("state") REFERENCES "public"."states" ("state") ON UPDATE RESTRICT ON DELETE RESTRICT,
     PRIMARY KEY ("id") , UNIQUE ("id")
 );
