23e48dfc8ef33e3e426c5c39eeaef2ac5e041e87
Laurent Michel <softozor@gmail.com>
2021-09-19 11:40:17 +0200

    define states before the todos table

--- features/data/database/todo_project/migrations/default/1632042013881_define_state_enum/down.sql
+++
@@ -1,5 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- INSERT INTO "public"."states"("state") VALUES (E'NEW'), (E'DOING'), (E'DONE');
-
-TRUNCATE TABLE states;
