94b9f0f2d85e46034354201679d4646b1e95c6c6
Laurent Michel <softozor@gmail.com>
2021-11-04 15:37:00 +0100

    clean up + make sure we can restore the nuget packages from the softozor nuget source

--- serverless/functions/Softozor.FusionAuth.TokenValidation/AppSetup.cs
+++ serverless/functions/Softozor.FusionAuth.TokenValidation/AppSetup.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Softozor.HasuraHandling;
