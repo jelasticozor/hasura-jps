f0eff48367cae5ab53e3f52fa83ed0869083ff6f
Laurent Michel <softozor@gmail.com>
2021-11-19 13:46:28 +0100

    simplify login function code

--- serverless/functions/Softozor.FusionAuth.Login/LoginProfile.cs
+++ serverless/functions/Softozor.FusionAuth.Login/LoginProfile.cs
@@ -7,7 +7,13 @@ public class LoginProfile : Profile
 {
     public LoginProfile()
     {
-        this.CreateMap<LoginInput, LoginRequest>().ConvertUsing<LoginRequestTypeConverter>();
-        this.CreateMap<LoginResponse, LoginOutput>().ConvertUsing<LoginOutputTypeConverter>();
+        this.CreateMap<LoginInput, LoginRequest>()
+            .ForMember(dest => dest.applicationId, opt => opt.MapFrom(src => src.AppId))
+            .ForMember(dest => dest.loginId, opt => opt.MapFrom(src => src.Username))
+            .ForMember(dest => dest.password, opt => opt.MapFrom(src => src.Password));
+
+        this.CreateMap<LoginResponse, LoginOutput>()
+            .ForMember(dest => dest.Token, opt => opt.MapFrom(src => src.token))
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.user.id.GetValueOrDefault()));
     }
 }
