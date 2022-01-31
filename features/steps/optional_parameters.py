3736ef5a13a7f9a78b2dafb1fe834637db3c6f85
Laurent Michel <softozor@gmail.com>
2022-01-31 18:00:37 +0100

    add some more debug info

--- features/steps/optional_parameters.py
+++ features/steps/optional_parameters.py
@@ -7,10 +7,12 @@ def step_impl(context, manifest_field):
     account_client = context.jelastic_clients_factory.create_account_client()
     user_info = account_client.get_user_info()
     expected_email = user_info.email()
-    assert expected_email == actual_email
+    assert expected_email == actual_email, \
+        f'expected {expected_email}, got {actual_email}'
 
 
 @then('the \'{manifest_field}\' contains {field_length} characters')
 def step_impl(context, manifest_field, field_length):
     actual_field_value = context.manifest_data[manifest_field]
-    assert len(actual_field_value) == field_length
+    assert len(actual_field_value) == field_length, \
+        f'expected {actual_field_value} to contain {field_length} characters, got {len(actual_field_value)}'
