fa4552700ec65ec905342333d1409172f92157bd
Laurent Michel <softozor@gmail.com>
2022-02-04 08:26:58 +0100

    define user management email templates in fusionauth

--- fusionauth/setup_kickstart.py
+++ fusionauth/setup_kickstart.py
@@ -22,7 +22,6 @@ def system_configuration(cors_allowed_origins):
 
 
 def main(args):
-    data = {}
     with open(args.input_kickstart) as file:
         data = json.load(file)
         data['variables']['adminEmail'] = args.admin_email
@@ -32,6 +31,8 @@ def main(args):
         data['variables']['issuer'] = args.issuer
         data['variables']['almightyApiKey'] = args.almighty_api_key
         data['variables']['serverlessApiKey'] = args.serverless_api_key
+        data['variables']['fromEmail'] = args.from_email
+        data['variables']['fromName'] = args.from_name
         if args.cors_allowed_origins:
             data['requests'].append(system_configuration(
                 args.cors_allowed_origins.replace(' ', '').split(',')))
@@ -55,10 +56,14 @@ if __name__ == '__main__':
     parser.add_argument('--hasura-claims-namespace',
                         default='https://hasura.io/jwt/claims', type=str, action='store')
     parser.add_argument('--issuer', required=True, type=str, action='store')
+    parser.add_argument('--from-email', required=True,
+                        type=str, action='store')
+    parser.add_argument('--from-name', required=True, type=str, action='store')
     parser.add_argument('--input-kickstart', required=True,
                         type=str, action='store')
     parser.add_argument('--output-kickstart', required=True,
                         type=str, action='store')
+
     args = parser.parse_args()
 
     main(args)
