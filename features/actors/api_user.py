0539acb991b1bf199490d676ad051ab6d686999a
Laurent <softozor@gmail.com>
2022-04-05 05:09:14 +0000

    stop accessing non-existent token field from sign_up action

--- features/actors/api_user.py
+++ features/actors/api_user.py
@@ -35,7 +35,6 @@ class ApiUser:
         graphql_response = self.__execute_graphql_query(
             query_name='sign_up', variables=variables)
         if 'errors' not in graphql_response.payload:
-            self.jwt = graphql_response.payload['data']['sign_up']['token']
             self.user_id = graphql_response.payload['data']['sign_up']['userId']
         return graphql_response
 
