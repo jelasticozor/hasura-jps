a331b04c7c7259da95284c065b80987c10faa3a8
Laurent Michel <softozor@gmail.com>
2022-04-14 11:41:50 +0200

    fix update_smtp script

--- fusionauth/update_smtp.py
+++ fusionauth/update_smtp.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 
+import requests
 from fusionauth.fusionauth_client import FusionAuthClient
 
 
@@ -23,7 +25,7 @@ def get_default_tenant_id(client):
     return default_tenant['id']
 
 
-def main(tenant_id, hostname, port, username, password, enable_ssl):
+def main(tenant_id, hostname, port, username, password, enable_ssl, api_url, api_key):
     request = {
         'tenant': {
             'emailConfiguration': {
@@ -35,8 +37,18 @@ def main(tenant_id, hostname, port, username, password, enable_ssl):
             }
         }
     }
-    response = client.update_tenant(tenant_id, request)
-    if not response.was_successful():
+    # TODO: no idea why this is not working:
+    # response = client.update_tenant(tenant_id, request)
+    # if not response.was_successful():
+    #     raise RuntimeError(
+    #         f'unable to update tenant with id {tenant_id} (status {response.status})')
+
+    # this is a work-around:
+    headers = {'content-type': 'application/json', 'authorization': api_key}
+    response = requests.patch(
+        f'{api_url}/api/tenant/{tenant_id}', data=json.dumps(request), headers=headers)
+
+    if response.status_code != 200:
         raise RuntimeError(
             f'unable to update tenant with id {tenant_id} (status {response.status})')
 
@@ -66,4 +78,6 @@ if __name__ == '__main__':
          args.mail_server_port,
          args.mail_server_username,
          args.mail_server_password,
-         args.mail_server_enable_ssl)
+         args.mail_server_enable_ssl,
+         args.api_url,
+         args.api_key)
